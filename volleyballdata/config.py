ffe73b3b68ba0e5bcab9b5b1e24e5c39c6579f5c
HHsieh09 <henryfbs01@gmail.com>
2025-05-19 00:12:14 -0400

    Modify Setting

--- volleyballdata/config.py
+++ volleyballdata/config.py
@@ -2,7 +2,7 @@ import os
 
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "127.0.0.1")
 MYSQL_USER = os.environ.get("MYSQL_USER","user")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD","test")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD","Henry71341_")
 MYSQL_PORT = int(os.environ.get("MYSQL_PORT","3306"))
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "volleyballdb")
 WORKER_ACCOUNT = os.environ.get("WORKER_ACCOUNT", "worker")
